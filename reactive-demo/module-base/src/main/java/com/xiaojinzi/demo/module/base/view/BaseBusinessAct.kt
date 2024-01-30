ead72dc3145799b841640fbe8ab120131413b127
xiaojiniz <347837667@qq.com>
2024-01-30 08:55:25 +0800

    Initial commit

--- reactive-demo/module-base/src/main/java/com/xiaojinzi/demo/module/base/view/BaseBusinessAct.kt
+++ reactive-demo/module-base/src/main/java/com/xiaojinzi/demo/module/base/view/BaseBusinessAct.kt
@@ -2,15 +2,35 @@ package com.xiaojinzi.demo.module.base.view
 
 import android.os.Bundle
 import androidx.annotation.CallSuper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
 import com.xiaojinzi.component.Component
-import com.xiaojinzi.reactive.view.BaseAct
 import com.xiaojinzi.reactive.view.BaseViewModel
 
-open class BaseBusinessAct<VM : BaseViewModel> : BaseAct<VM>() {
+open class BaseBusinessAct<VM : BaseViewModel> : AppCompatActivity() {
+
+    /*上下文*/
+    protected lateinit var mContext: FragmentActivity
+
+    protected var mViewModel: VM? = null
+
+    protected open fun getViewModelClass(): Class<VM>? {
+        return null
+    }
+
+    fun requiredViewModel(): VM {
+        return mViewModel!!
+    }
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mContext = this
+        val viewModelClass = getViewModelClass()
+        if (viewModelClass != null) {
+            mViewModel = ViewModelProvider(this)[viewModelClass]
+        }
         Component.inject(this)
     }
 
--- reactive/src/main/java/com/xiaojinzi/reactive/view/BaseViewModel.kt
+++ reactive/src/main/java/com/xiaojinzi/reactive/view/BaseViewModel.kt
@@ -13,12 +13,10 @@ import com.xiaojinzi.reactive.domain.BaseUseCase
  * [Subject] 中和 [LiveData] 对应的是 [BehaviorSubject]
  * 都是表示订阅最近发射出来的一个信号. 如果只想收到订阅之后的,
  * 请使用 [PublishSubject]. 整体其实 Google 官方的是不够的, 只有一个
- * [LiveData] 模式. 总体上 RxJava 的模式比较全,
- * 具体可以去看 [Subject] 这个抽象类的实现
+ * [LiveData] 模式. Kotlin 的 Flow 出现, 弥补了 Google 响应式这块的不足
+ * [SharedFlow] 和 [SharedStateFlow] 都是很好的选择
  *
  * @Note: ViewModel 不是用来承载业务逻辑的, 只是适合处理 UI 相关的数据和保存 UI 相关的数据及其需要保存的引用
- * @see BaseAct
- * @see BaseFrag
  */
 abstract class
 BaseViewModel : ViewModel() {
